9c56f5f18b1194de251c50a2971480549e50906f
Md Imran Alam <mdimranalam2348@gmail.com>
2024-09-18 16:46:02 +0530

    Array Practice Program

---
+++ 01_one dimensional array/FirstArrayProgram.java
@@ -0,0 +1,24 @@
+public class FirstArrayProgram {
+    public static void main(String[] args) {
+
+       /** create Integer Array*/
+
+       int [] arr= new int[5];
+
+       /** Array input*/
+        arr[0]=12;
+        arr[1]=85;
+        arr[2]=96;
+        arr[3]=45;
+        arr[4]=22;
+
+        /** print the all Array Element*/
+
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+        System.out.println(arr[3]);
+        System.out.println(arr[4]);
+
+    }
+}
