3554530a6c3ac93b9f821bc67f0ffd8c50e5cd8b
Md Imran Alam <mdimranalam2348@gmail.com>
2024-09-26 18:00:07 +0530

    Array Example

---
+++ 01_one dimensional array/Example_1.java
@@ -0,0 +1,24 @@
+public class Example_1 {
+
+    public static void main(String[] args) {
+
+        int array [] = {45,65,21,65,98,45,25,65};
+
+       /** System.out.println(array[0]);
+        System.out.println(array[1]);
+        System.out.println(array[2]);
+        System.out.println(array[3]);
+        System.out.println(array[4]);
+        System.out.println(array[6]);
+        System.out.println(array[7]); */
+
+        /**print Array Using the for loop*/
+
+        for (int i=0;i< array.length;i++){
+            System.out.println(array[i]);
+        }
+
+
+    }
+    
+}
